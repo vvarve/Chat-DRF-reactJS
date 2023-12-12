4ce643077a1c8e9ed83fee3810223215f95a26fe
vvarve <vvarve@gmail.com>
2023-12-12 15:07:33 -0400

    hour chats

--- app/models.py
+++ app/models.py
@@ -14,5 +14,5 @@ class chat(models.Model):
     text = models.CharField(max_length=300, null=True, blank=True)
     files = models.FileField(upload_to="files/", null=True, blank=True)
     images = models.FileField(upload_to="images/", null=True, blank=True)
-    date = models.DateField(auto_now=True, null=True, blank=True)
+    date = models.DateTimeField(auto_now=True, null=True, blank=True)
     user_two = models.ForeignKey(Username, on_delete=models.CASCADE,  related_name="user_two")
